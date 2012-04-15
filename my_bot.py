58f971a6f6f1e3b90a97039459df66710c9a403f
Brendon Justin <SNES350@gmail.com>
2012-04-14 20:33:18 -0400

    Initial commit of provided files

---
+++ my_bot.py
@@ -0,0 +1,94 @@
+import random
+
+import traders
+import run_experiments
+import plot_simulation
+
+class MyBot(traders.Trader):
+    name = 'my_bot'
+
+    def simulation_params(self, timesteps,
+                          possible_jump_locations,
+                          single_jump_probability):
+        """Receive information about the simulation."""
+        # Number of trading opportunities
+        self.timesteps = timesteps
+        # A list of timesteps when there could be a jump
+        self.possible_jump_locations = possible_jump_locations
+        # For each of the possible jump locations, the probability of
+        # actually jumping at that point. Jumps are normally
+        # distributed with mean 0 and standard deviation 0.2.
+        self.single_jump_probability = single_jump_probability
+        # A place to store the information we get
+        self.information = []
+    
+    def new_information(self, info, time):
+        """Get information about the underlying market value.
+        
+        info: 1 with probability equal to the current
+          underlying market value, and 0 otherwise.
+        time: The current timestep for the experiment. It
+          matches up with possible_jump_locations. It will
+          be between 0 and self.timesteps - 1."""
+        self.information.append(info)
+
+    def trades_history(self, trades, time):
+        """A list of everyone's trades, in the following format:
+        [(execution_price, 'buy' or 'sell', quantity,
+          previous_market_belief), ...]
+        Note that this isn't just new trades; it's all of them."""
+        self.trades = trades
+
+    def trading_opportunity(self, cash_callback, shares_callback,
+                            check_callback, execute_callback,
+                            market_belief):
+        """Called when the bot has an opportunity to trade.
+        
+        cash_callback(): How much cash the bot has right now.
+        shares_callback(): How many shares the bot owns.
+        check_callback(buysell, quantity): Returns the per-share
+          price of buying or selling the given quantity.
+        execute_callback(buysell, quantity): Buy or sell the given
+          quantity of shares.
+        market_belief: The market maker's current belief.
+
+        Note that a bot can always buy and sell: the bot will borrow
+        shares or cash automatically.
+        """
+        # Place a randomly sized trade in the direction of
+        # our last information. What could possibly go wrong?
+        quantity = random.choice(xrange(1, 100))
+        if (self.information[-1] == 1
+            and check_callback('buy', quantity) < 99.0):
+            execute_callback('buy', quantity)
+        elif check_callback('sell', quantity) > 1.0:
+            execute_callback('sell', quantity)
+
+def main():
+    bots = [MyBot()]
+    
+    # Plot a single run. Useful for debugging and visualizing your
+    # bot's performance. Also prints the bot's final profit, but this
+    # will be very noisy.
+    plot_simulation.run(bots)
+    
+    # Calculate statistics over many runs. Provides the mean and
+    # standard deviation of your bot's profit.
+    #run_experiments.run(bots, simulations=2000)
+
+# Extra parameters to plot_simulation.run:
+#   timesteps=100, lmsr_b=150
+
+# Extra parameters to run_experiments.run:
+#   timesteps=100, num_processes=2, simulations=2000, lmsr_b=150
+
+# Descriptions of extra parameters:
+# timesteps: The number of trading rounds in each simulation.
+# lmsr_b: LMSR's B parameter. Higher means prices change less,
+#           and the market maker can lose more money.
+# num_processes: In general, set this to the number of cores on your
+#                  machine to get maximum performance.
+# simulations: The number of simulations to run.
+
+if __name__ == '__main__': # If this file is run directly
+    main()
